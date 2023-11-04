0cdfacff8124cded6f676ad2139ec77441965ed3
keko <adelquipompei@gmail.com>
2023-11-03 22:58:12 -0300

    todo el box y los iconos terminados , me voy a comer el asado

--- js/script.js
+++ js/script.js
@@ -1,40 +1,45 @@
+
+
 let menu = document.getElementById('menu');
 
 
 
-function mostrarMenu(){
-    let menuAbierto=false;
-    menu.style.width='100%';
-    menu.style.padding='20px';
+function mostrarMenu() {
+
+    menu.style.width = '100%';
+    menu.style.padding = '20px';
     document.getElementById('men').classList.add('d-none');
     document.getElementById('cerrar').classList.remove('d-none');
-    document.body.style.overflowY='hidden';
+    document.body.style.overflowY = 'hidden';
 }
 
-function cerrarMenu(){
-   menu.style.width='0%';
-   menu.style.padding='0';
-   document.getElementById('men').classList.remove('d-none');
+function cerrarMenu() {
+    menu.style.width = '0';
+    menu.style.padding = '0';
+    document.getElementById('men').classList.remove('d-none');
     document.getElementById('cerrar').classList.add('d-none');
-    document.body.style.overflowY='visible';
+    document.body.style.overflowY = 'visible';
 }
 
-function mostrarCaja(bxRopaId,imagenID){
+function mostrarCaja(bxRopaId, imagenID) {
     let bxRopa = document.getElementById(bxRopaId)
     let imagen = document.getElementById(imagenID)
-    bxRopa.style.display='flex';
+    bxRopa.style.display = 'flex';
     imagen.classList.add('shadow');
-    
-    
-}
- 
+    let shop = document.getElementsByClassName('ti-shopping-cart'); 
+   
 
-function cerrarCaja(bxRopaId,imagenID){
+
+
+
+}
+
+
+function cerrarCaja(bxRopaId, imagenID) {
     let bxRopa = document.getElementById(bxRopaId)
     let imagen = document.getElementById(imagenID)
-    bxRopa.style.display='none';
+    bxRopa.style.display = 'none';
     imagen.classList.remove('shadow');
-    
+
 }
 
-    
